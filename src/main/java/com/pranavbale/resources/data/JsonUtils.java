62a0955f616846d4d230158a2c35d135641a962a
Pranav Bale <pranavbale99@gmail.com>
2025-11-28 00:41:21 +0530

    working on Register User

--- src/main/java/com/pranavbale/resources/data/JsonUtils.java
+++ src/main/java/com/pranavbale/resources/data/JsonUtils.java
@@ -4,6 +4,9 @@ import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
@@ -11,30 +14,16 @@ public class JsonUtils {
 
     public static ObjectMapper mapper = new ObjectMapper();
 
-//    public static List<Map<String, String>> readJsonArray(String resourceName) throws Exception {
-//        // Use package-relative lookup: "Users.json" (no leading slash)
-//        try (InputStream is = JsonUtils.class.getResourceAsStream(resourceName)) {
-//            if (is == null) {
-//                // helpful error message showing what was attempted
-//                String pkgPath = JsonUtils.class.getPackage().getName().replace('.', '/');
-//            }
-//            return mapper.readValue(is, new TypeReference<List<Map<String, String>>>() {});
-//        }
-//    }
 
-    public static List<Map<String, String>> readJsonArray(String resourceName) throws Exception {
-        try (InputStream is = JsonUtils.class.getResourceAsStream("/" + resourceName)) {
-            if (is == null) {
-                throw new IllegalArgumentException("Resource not found: " + resourceName);
-            }
-
-            return mapper.readValue(is, new TypeReference<List<Map<String, String>>>() {});
-        }
+    public static List<Map<String, String>> readJsonArray(String filename) throws Exception {
+        Path path = Paths.get("src/main/java/com/pranavbale/resources/data/" + filename);
+        String json = Files.readString(path);
+        return mapper.readValue(json, new TypeReference<List<Map<String, String>>>() {});
     }
 
     public static void main(String[] args) throws Exception {
-        List<Map<String, String>> data =  readJsonArray("/Users.json");
-        String name = data.getFirst().get("firstName");
+        List<Map<String, String>> data =  readJsonArray("Users.json");
+        String name = data.get(0).get("email");
         System.out.println(name);
     }
 }
